dd9714ba83cdf0ead156254011ba2d64f002e69d
lucy <lucy@HELVETIA>
2025-01-25 18:17:07 -0800

    Add project files.

---
+++ taskify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace taskify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
